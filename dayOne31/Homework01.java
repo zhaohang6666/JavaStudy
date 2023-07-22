9db9a1a63de52b02edf78d5f3208971fb56c0669
zhaohang6666 <134931874+zhaohang6666@users.noreply.github.com>
2023-07-22 21:16:25 +0800

    Add files via upload

---
+++ dayOne31/Homework01.java
@@ -0,0 +1,8 @@
+package dayOne31;
+//将个人基本信息（姓名、性别、籍贯、住址）打印到控制台
+public class Homework01 {
+    public static void main(String[] args){
+        System.out.println("姓名\t性别\t籍贯\t住址\n赵航\t男\t山东\t嘉祥");
+    }
+
+}
